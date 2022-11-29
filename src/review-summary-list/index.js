8a42424b4ee6935ee76c9f4f65bf4c55613b1d17
Hantong Liu <hantongliu@outlook.com>
2022-11-29 10:53:16 -0800

    generalize reviews list

--- src/review-summary-list/index.js
+++ src/review-summary-list/index.js
@@ -4,13 +4,12 @@ import { useEffect } from "react";
 import { findReviewsThunk } from "../services/reviews-thunk";
 import PostReview from "./review-post";
 
-const ReviewSummaryList = () => {
+const ReviewSummaryList = ({title, username, imdbID, length}) => {
   // const { currentUser } = useSelector((state) => state.users);
   const { reviews, loading } = useSelector((state) => state.reviews);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(findReviewsThunk());
-    // eslint-disable-next-line
   }, []);
   const defaultCol = {
     username: "Username",
@@ -24,15 +23,16 @@ const ReviewSummaryList = () => {
       {/*{currentUser && <h2>Welcome {currentUser.username}</h2>}*/}
       <ul className="list-group border border-primary mt-3">
         <li className="list-group-item fw-bold">
-          <h4>Recent Hot Reviews</h4>
+          <h4>{title}</h4>
         </li>
         {loading && <li className="list-group-item">Loading...</li>}
         {!loading && (
           <div>
             <ReviewSummaryListItem review={defaultCol} />
             {reviews.map((review) => (
+                (review.username === username || review.imdbID === imdbID || (username === undefined && imdbID === undefined)) &&
               <ReviewSummaryListItem review={review} />
-            ))}
+            )).slice(0, length)}
           </div>
         )}
       </ul>
