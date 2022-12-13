e384498c336930e5adaac4b9395ae96670051e73
Hantong Liu <hantongliu@outlook.com>
2022-12-12 19:42:21 -0800

    change date format

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,12 +1,15 @@
+function transform(num) {
+    return num >= 10 ? num : "0" + num;
+}
+
 export const timeConverter = (UNIX_timestamp) => {
     var a = new Date(UNIX_timestamp);
     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
+    var date = transform(a.getDate());
+    var hour = transform(a.getHours());
+    var min = transform(a.getMinutes());
     var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
     return time;
 }
